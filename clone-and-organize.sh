40237f62965221cb458cdfa005869e1c5511303d
Igor Popov <igor.v.popov@outlook.com>
2021-07-17 13:43:24 +0300

    1/ Готов скрипт который клонирует нужные репозитории и создает симлинки  / 2/ Готов скрипт который создает заметку с меткой времени  в качестве названия и первой линии

---
+++ clone-and-organize.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env sh
+
+arg0=$1 # prefix: `work` or `mine` or etc
+arg1=$2 # key for repository, for example: `zvq-etl`
+
+if [ "$arg0" == 'work' ];
+    then arg3='zvuk'
+elif [ "$arg0" == 'cotwo' ];
+    then arg3='cotwo-project'
+elif [ "$arg0" == 'mine' ];
+    then arg3='orsk-moscow'
+else 
+    echo please set correct arg0; 
+    exit 1;
+fi
+
+test "$arg1" == '' && (echo please set an arg1; exit 1)
+
+repo='git@github.com:'$arg3'/'$arg1'.git' 
+src='/Users/'$(whoami)'/'$arg0'-'$arg1
+dest='/Users/'$(whoami)'/'$arg0'-'$arg1'.lnk'
+
+git clone $repo $src
+ln -s $src $dest
