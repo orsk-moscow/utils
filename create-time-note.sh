eb8d4bbf4f03b5ebc1f2dffef9f515b975ff3908
Igor Popov <igor.v.popov@outlook.com>
2021-07-25 12:35:18 +0300

    1/ откат изменений на 1 коммит

--- create-time-note.sh
+++ create-time-note.sh
@@ -6,7 +6,7 @@ if [ "$arg0" == '' ];
     then arg0="/Users/$(whoami)/Yandex.Disk.localized/-text-notes/"
 fi
 
-dt1=$(date '+%Y-%m-%d %H:%M')
+dt1=$(date '+%Y-%m-%d-%H:%M')
 filename=$arg0$dt1.txt
 dt2=$(date '+%Y-%m-%d %H:%M')
 echo $dt2 > $filename && open $filename
