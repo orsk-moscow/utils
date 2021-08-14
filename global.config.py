ed40dd677bc822cc5931c9ea8ed0beb1c69506f6
Igor Popov <igor.v.popov@outlook.com>
2021-08-14 16:11:53 +0300

    1/ добавил 2 базовых python файла

---
+++ global.config.py
@@ -0,0 +1,2 @@
+DEBUG = True
+BASE_FILE_FORMAT = "%Y%m%d-%H%M"
---
+++ utils.py
@@ -0,0 +1,44 @@
+from path import Path
+import logging
+from datetime import datetime
+from path import Path
+from config import BASE_FILE_FORMAT
+
+
+def validate_path(path_obj, endswith):
+    if not path_obj.exists():
+        logging.error(f"""filename '{path_obj}' does not exists""")
+    elif not path_obj.isfile():
+        logging.error(f"""filename '{path_obj}' is not a file""")
+    elif not path_obj.endswith(endswith):
+        logging.error(f"""filename '{path_obj}' is not a '*.{endswith}' file""")
+    else:
+        return path_obj
+    exit()
+
+
+def make_logging_config(debug=False):
+    logdir = Path("debug" if debug else "logs")
+    logdir.mkdir_p()
+    logfile = Path.joinpath(
+        logdir,
+        f"""{datetime.today().strftime(BASE_FILE_FORMAT)}{"-DEBUG"if debug else ""}.log""",
+    )
+    if debug:
+        logging.basicConfig(
+            level=logging.DEBUG,
+            format="%(asctime)s %(levelname)s %(name)s %(funcName)s: %(message)s",
+            filename=logfile,
+            force=True,
+        )
+        logging.debug("this is test run")
+    else:
+        logging.basicConfig(
+            level=logging.INFO,
+            format="%(asctime)s %(levelname)s %(name)s %(funcName)s: %(message)s",
+            filename=logfile,
+            force=True,
+        )
+        logging.info("this is production run")
+    return
+
