3448171da07e4de4918b13bc7edb0245b0aa5bd5
Igor Popov <igor.v.popov@outlook.com>
2021-09-16 00:01:24 +0300

    1/ debug to False/

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 from datetime import datetime
 
-DEBUG = True
+DEBUG = False
 BASE_FILE_FORMAT = "%Y%m%d-%H%M"  # means "yyyyMMdd-HHmm", example: "20210909-0750"
 BASE_FORMAT_DATE = "%Y%m%d"
 BASE_FORMAT_TIME = "%H%M"
