7c0a129baa08261d6f49b649b42d13dbfa098f0d
Igor Popov <igor.v.popov@outlook.com>
2022-01-08 22:44:49 +0300

    1/ + метка времени в префиксе файла/

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ from numpy import nan
 from pandas import NaT as nat
 
 DEBUG = False
-BASE_FILE_FORMAT = "%Y%m%d-%H%M"  # means "yyyyMMdd-HHmm", example: "20210909-0750"
+BASE_FILE_FORMAT = "%Y%m%d-%H%M-"  # means "yyyyMMdd-HHmm-", example: "20210909-0750-"
 BASE_FORMAT_DATE = "%Y%m%d"
 BASE_FORMAT_TIME = "%H%M"
 DATETIME_PREFIX = datetime.now().strftime(BASE_FILE_FORMAT)
